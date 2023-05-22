659e852c30791d09eda20d7f82890744df473691
wesleymth <wesley.monteith-finas@epfl.ch>
2023-05-22 12:02:55 +0200

    feat: add python func for frame detection

--- video_processing.py
+++ video_processing.py
@@ -1,4 +1,5 @@
 import datetime
+import subprocess
 from datetime import timedelta
 
 from moviepy.editor import VideoFileClip, concatenate_videoclips
@@ -59,12 +60,33 @@ def clean_video(
     video_clip.close()
 
 
+def detect_frame_changes(
+    cleaned_vieo_path: str,
+    metadata_output_path: str,
+    frames_output_path: str,
+    sensitivity_threshold: float = 0.05,
+) -> None:
+    bash_command = f"""ffmpeg -i {cleaned_vieo_path} -filter_complex "select='gt(scene,{str(sensitivity_threshold)})',metadata=print:file={metadata_output_path}" -start_number 0 -fps_mode vfr {frames_output_path}"""
+    # ffmpeg -i 'data/syn_bio_rna_lecture_1/BIOENG320-lecture1(2023)_Prof-Yimon-AYE.mp4' -filter_complex "select='gt(scene,0.05)',metadata=print:file=output/BIOENG320-lecture1(2023)_Prof-Yimon-AYE/frame_changes.txt" -start_number 0 -fps_mode vfr 'output/BIOENG320-lecture1(2023)_Prof-Yimon-AYE/frames/frame-%03d.png'
+    print(
+        f"\nDetecting frame changes in video {cleaned_vieo_path} using the command:\n{bash_command}"
+    )
+    process = subprocess.Popen(bash_command.split(), stdout=subprocess.PIPE)
+    output, error = process.communicate()
+
+
 if __name__ == "__main__":
-    clean_video(
-        mp4_path="data/BIOENG320_lecture2(2023)_Prof-Yimon-AYE.mp4",
-        clean_video_path="output/cleaned_BIOENG320_lecture2(2023)_Prof-Yimon-AYE.mp4",
-        video_start=timedelta(hours=0, minutes=10, seconds=5),
-        video_end=timedelta(hours=1, minutes=56, seconds=4),
-        pause_start_time=timedelta(hours=1, minutes=00, seconds=18),
-        pause_end_time=timedelta(hours=1, minutes=13, seconds=10),
+    # clean_video(
+    #     mp4_path="data/BIOENG320_lecture2(2023)_Prof-Yimon-AYE.mp4",
+    #     clean_video_path="output/cleaned_BIOENG320_lecture2(2023)_Prof-Yimon-AYE.mp4",
+    #     video_start=timedelta(hours=0, minutes=10, seconds=5),
+    #     video_end=timedelta(hours=1, minutes=56, seconds=4),
+    #     pause_start_time=timedelta(hours=1, minutes=00, seconds=18),
+    #     pause_end_time=timedelta(hours=1, minutes=13, seconds=10),
+    # )
+    detect_frame_changes(
+        cleaned_vieo_path="data/syn_bio_rna_lecture_1/BIOENG320-lecture1(2023)_Prof-Yimon-AYE.mp4",
+        metadata_output_path="output/BIOENG320-lecture1(2023)_Prof-Yimon-AYE/frame_changes.txt",
+        frames_output_path="output/BIOENG320-lecture1(2023)_Prof-Yimon-AYE/frames/frame-%03d.png",
+        sensitivity_threshold=0.05,
     )
